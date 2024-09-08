438ebf776aaaf11d29b28ab0b139bbaa5f0b3e8e
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-09-08 10:58:14 +0330

    cooming soon !

---
+++ tamrin-13/js/script.js
@@ -0,0 +1,42 @@
+﻿//Short cut
+let $ = document
+//Selector Elements
+let listElem = $.querySelector('.list')
+let listItemsElem = $.querySelectorAll('.list-items')
+let inputElem = $.querySelector('.input-task')
+let trashIcon = $.querySelectorAll('.bi-trash')
+let checkboxIcon = $.querySelectorAll('.checkbox')
+let icons = $.querySelectorAll('.icons')
+
+inputElem.addEventListener("keydown", function (event) {
+
+  if (event.key === 'Enter') {
+    // console.log('test')
+    let ValueInput = inputElem.value
+    let newItems = $.createElement("li")
+    let Itemsspan = $.createElement("span")
+    let Itemsdiv = $.createElement("div")
+    let Itag1 = $.createElement("i")
+    let Itag2 = $.createElement("i")
+
+    Itemsdiv.append(Itag1, Itag2)
+    newItems.append(Itemsspan, Itemsdiv)
+
+    Itemsspan.innerHTML = ValueInput
+    newItems.classList = "list-items"
+    Itemsdiv.classList = "icons"
+    Itag1.classList = "bi bi-app checkbox"
+    Itag2.classList = "bi bi-trash"
+
+    listElem.append(newItems)
+    inputElem.value = ''
+  }
+
+})
+
+icons.forEach(function (item) {
+  let icons = $.querySelectorAll('.icons')
+  item.lastElementChild.addEventListener('click', function (event) {
+    event.target.parentElement.parentElement.remove()
+  })
+})
