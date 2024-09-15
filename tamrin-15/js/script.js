20ec4b8276633fdc83822d4dc4789b9732a45cee
Abolfazl Mohseni <131040476+abolfazlmohseni@users.noreply.github.com>
2024-09-15 17:01:26 +0330

    final

--- tamrin-15/js/script.js
+++ tamrin-15/js/script.js
@@ -1,12 +1,31 @@
-let contextMenu = document.querySelector(".context-menu")
-let bodyElem = document.querySelector("body")
+﻿const contextMenu = document.getElementById('contextMenu')
 
-bodyElem.addEventListener('contextmenu', function (event) {
-    var Xposion = event.pageX
-    var Yposion = event.pageY
-    contextMenu.style.top = Xposion
-    contextMenu.style.left = Yposion
+function contextHandler(event) {
+  event.preventDefault()
+
+  if (contextMenu.style.display === 'none') {
+
+    contextMenu.style.left = event.pageX + 'px'
+    contextMenu.style.top = event.pageY + 'px'
     contextMenu.style.display = 'block'
+  } else {
+    contextMenu.style.left = event.pageX + 'px'
+    contextMenu.style.top = event.pageY + 'px'
+  }
 
-})
+  console.log(event);
+}
 
+function clickContextMenu() {
+  contextMenu.style.display = 'none'
+}
+
+function keyDownHandler(event) {
+  if (event.keyCode === 27)
+    contextMenu.style.display = 'none'
+}
+
+
+document.body.addEventListener('contextmenu', contextHandler)
+document.body.addEventListener('click', clickContextMenu)
+document.body.addEventListener('keydown', keyDownHandler)
